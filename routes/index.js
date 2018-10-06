7935baec38af68c49ebcdb000e75613eec6da2c4
Kris Steigerwald <kris.steigerwald@gmail.com>
2018-10-06 00:49:05 -0700

    webhok-handler 1

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.status(200).send("Ok");
 });
 
 router.get('/ping', function(req, res, next) {
   res.status(200).send("Ok");
 });
+
 module.exports = router;
