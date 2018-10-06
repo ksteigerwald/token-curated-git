67fea0110551b6bf7b08e9f5ef4fc3acd0c95e71
Kris Steigerwald <kris.steigerwald@gmail.com>
2018-10-06 00:18:02 -0700

    - scaffold api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send("Ok");
 });
 
+router.get('/ping', function(req, res, next) {
+  res.status(200).send("Ok");
+});
 module.exports = router;
