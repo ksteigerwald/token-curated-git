b0c1b3f2b59d154e41ebd7ba61f84eff4441a840
Kris Steigerwald <kris.steigerwald@gmail.com>
2018-10-06 09:42:03 -0700

    stub commit

--- routes/github.api.js
+++ routes/github.api.js
@@ -5,7 +5,9 @@ octokit.authenticate({
   token: process.env.GHTOKEN
 });
 //octokit.repos.createDeploymentStatus('ETHSF', 'token-curated-git', 1, 'pending', '', '', '', '', '', (error, result) => {})
-var opts = {owner:'ETHSF', repo:'token-curated-git', deployment_id:1, state:'pending'}
-octokit.repos.createDeploymentStatus(opts).then(result => {
+var opts = {owner:'ETHSF', repo:'token-curated-git', sha:'063a8b0430277c15ff6026d0a53c2104e24a11f5', state:'pending'}
+octokit.repos.createStatus(opts).then(result => {
   console.log(opts);
+}).catch(err => {
+ console.log("ERROR:", err)
 })
