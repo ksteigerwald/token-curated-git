70eb404b7871bbd4ea216ca585db6552f2dfeb51
Kris <kris.steigerwald@gmail.com>
2018-10-06 10:22:54 -0700

    Proto bot (#3)
    * Added app
    
    * cli
    
    * intorduce protobot
    
    * update to github app
    
    * add probot

---
+++ index.js
@@ -0,0 +1,32 @@
+// A plugin is a Node module that exports a function which takes a `robot` argument
+module.exports = robot => {
+
+  // Listen for a pull request being opened or synchronized
+  robot.on('pull_request', async (event, context) => {
+    // Just assign a variable to make our life easier
+    const pr = event.payload.pull_request;
+
+    // Get all the commits in the pull request
+    const compare = await context.github.repos.compareCommits(context.repo({
+      base: pr.base.sha,
+      head: pr.head.sha
+    }));
+
+    // Check that every commit has the word bananas in it
+    const isBananas = compare.commits.every(data => {
+      return data.commit.message.match(/bananas/i);
+    });
+
+    // Parameters for the status API
+    const params = {
+      sha: pr.head.sha,
+      context: 'bananas',
+      state: isBananas ? 'success' : 'failure',
+      description: `Your commits ${isBananas ? 'are' : 'need more'} bananas`
+    }
+
+    // Create the status
+    return context.github.repos.createStatus(context.repo(params));
+  });
+
+};
